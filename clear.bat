2056a59b0c13b0b4b5e78f08573c07c165ddef2e
Valentin Bombin <wwall@yandex.ru>
2014-12-24 13:27:08 +0300

    Changed 24.12.2014 13:27:08,08

--- clear.bat
+++ clear.bat
@@ -2,3 +2,4 @@ del *.html
 del *.bak
 del *.css
 del *.js
+del *.md
--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 call clear.bat
 
-::scribble.exe --html part1.scrbl
+
 scribble.exe --html part2.scrbl
+scribble.exe --markdown part2.scrbl
 start part2.html
